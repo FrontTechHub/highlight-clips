52d3c06b156445766b3432e27ed42426b7c47f84
huyhoang1001 <huyhoangvo1001@gmail.com>
2023-06-06 00:47:45 +0700

    Add validator for input fields

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -11,11 +11,28 @@ export class RegisterComponent {
     Validators.required,
     Validators.minLength(5),
   ]);
-  email= new FormControl('');
-  age= new FormControl('');
-  password= new FormControl('');
-  confirm_password= new FormControl('');
-  phone_number= new FormControl('');
+  email= new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  age= new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ]);
+  password= new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ]);
+  confirm_password= new FormControl('',[
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ]);
+  phone_number= new FormControl('', [
+    Validators.required,
+    Validators.minLength(10),
+    Validators.maxLength(10)
+  ]);
   registerForm = new FormGroup({
     name: this.name,
     email: this.email,
@@ -24,4 +41,8 @@ export class RegisterComponent {
     confirm_password: this.confirm_password,
     phone_number: this.phone_number,
   });
+
+  register() {
+
+  }
 }
